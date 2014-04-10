0911a15435995a3efb8c9a08846bc17c5461fd07
PixelPips <pips@pixelbyte.net>
2014-04-09 19:50:07 -0600

    7/9/2014

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
-    <!DOCTYPE html>
+<!DOCTYPE html>
 
-<?php
+<head>
     
-    include( "loader.php" );
-    loadPlayerInfo();
-    $playerInfo = getPlayerInfo();
-    echo ( $playerInfo['responce']['players']['personaname'] );
+<meta charset="UTF-8">
 
+</head>
+<body>
+
+<?php 
+
+$data = json_decode(file_get_contents('https://ttt-fun.com/staff/?json=1'), true);
+
+for ($i = 0; $i < count($data); $i++){
+    echo($data[$i]["nick"] . "<br><b>Time</b>" . date("W/d/h/m", $data[$i]["onlinetime"]) . "<br><br>");
+}
 ?>
-        
-    </body>
+
+</body>
