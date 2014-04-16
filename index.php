9bf40a209554f82ef7d0471a5c28d0c4a8b7453c
PixelPips <pips@pixelbyte.net>
2014-04-16 10:39:22 -0600

    +Saving
    + added support for hourly saving
    + added save timing
    + started to add documentation

--- index.php
+++ index.php
@@ -3,16 +3,23 @@
 <?php 
 include("methods.php");
 
+/*
+    Save function, will run every 24 hours or every hour depending on crontab status
+*/
+
 if ($_GET["action"] == "save"){
+    echo("Save started at " . date('i:s'));
+    
     $data = json_decode(file_get_contents('https://ttt-fun.com/staff/?json=1'), true);
     
     for ($i = 0; $i < count($data); $i++){
         $id2time[id264($data[$i]['steam'])] = $data[$i]['onlinetime'];
     }   
     
-    $fp = fopen("saves/".date('Y-m-d'). ".json", 'w');
+    $fp = fopen("saves/".date('Y-m-d@H'). ".json", 'w');
     fwrite($fp, json_encode($id2time));
     fclose($fp);
+    echo("<br>Save finished at ". date('i:s'));
 }
 
 if ($_GET["action"] == "load"){
@@ -20,6 +27,7 @@ if ($_GET["action"] == "load"){
     echo( file_get_contents("http://pb-mc.net/a/saves/". $date . ".json"));
     
 }
+
 ?>
 
 </body>
