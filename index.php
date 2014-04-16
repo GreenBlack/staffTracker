12dbca2ace490532c3aa2488b8dd023620e4eae6
PixelPips <pips@pixelbyte.net>
2014-04-16 12:43:15 -0600

    4/16/2014

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
 <meta charset="UTF-8">
 <?php 
+ 
+//major dependency
 include("methods.php");
 
 /*
@@ -8,26 +10,30 @@ include("methods.php");
 */
 
 if ($_GET["action"] == "save"){
+    
     echo("Save started at " . date('i:s'));
     
     $data = json_decode(file_get_contents('https://ttt-fun.com/staff/?json=1'), true);
-    
     for ($i = 0; $i < count($data); $i++){
-        $id2time[id264($data[$i]['steam'])] = $data[$i]['onlinetime'];
+        $id2time[steamID_steam64($data[$i]['steam'])] = $data[$i]['onlinetime'];
     }   
     
     $fp = fopen("saves/".date('Y-m-d@H'). ".json", 'w');
     fwrite($fp, json_encode($id2time));
     fclose($fp);
+    
     echo("<br>Save finished at ". date('i:s'));
 }
 
+/*
+    Load function. EXAMPLE: http://pb-mc.net/a/?action=load&date=2014-04-16@09
+*/
+
 if ($_GET["action"] == "load"){
-    $date=$_GET["date"];
-    echo( file_get_contents("http://pb-mc.net/a/saves/". $date . ".json"));
     
+    if (isset($_GET["date"])){
+    echo( file_get_contents("http://pb-mc.net/a/saves/". $_GET["date"] . ".json"));
+    }
 }
 
 ?>
-
-</body>
--- methods.php
+++ methods.php
@@ -1,6 +1,19 @@
 <?php
-
-function id264($steamID){
+/*
+    Converts $steamID into steam64 and returns it
+*/
+function steamID_steam64($steamID){
+    
      list($e, $type, $id) = split(":", $steamID);
     return ($id * 2) + 76561197960265728 + $type;
+    
 }
+
+/*
+    Gets player.json from valve with $steam64 then returns it decoded
+*/
+function steam64_json($steam64){
+    return json_decode(file_get_contents('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=CA7B8780E22D27C381BD47BF41565B27&steamids='. strval($steam64)), true);
+    
+}
+
