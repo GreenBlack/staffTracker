db11b2353902f52685eefafdc1ac40a290693871
PixelPips <pips@pixelbyte.net>
2014-05-06 00:30:38 -0600

    Documentation

--- index.php
+++ index.php
@@ -1,54 +1,215 @@
 <?php 
-header('Content-Type: charset=utf-8');
 //major dependency
 include("methods.php");
 
-/*
-    Save function, will run every 24 hours or every hour depending on crontab status. impliments IP security
-*/
+//AccessIP defines the IP(s) that are allowed to access alterior scrips, like saving.
+$accessIP = ["208.146.35.21"];
 
-if ($_GET["action"] == "save"){
+//check if IP address(s) in $accessIP are the incoming connectionw
+if (in_array ($_SERVER['REMOTE_ADDR'], $accessIP)) 
+{
+	if ($_GET["action"] == "save")
+		
+		/*
+			Save function. Runs hourly from a crontabs curl function running hourly.
+		*/
+		
+	{
+		echo("\nSave started at " . date('i:s'));
     
-    $accessIP = "208.146.35.21";
+		//load staff data from TTT-Fun
+    	$data = json_decode(file_get_contents('https://ttt-fun.com/staff/?json=1'), true);
+
+		//loop through the TTT-Fun data and add it to a Dictionary array
+    	for ($i = 0; $i < count($data); $i++)
+		{
+        	$savef[$i]["steamID"] = steamID_steam64($data[$i]['steam']);
+	    	$savef[$i]["time"] = $data[$i]['onlinetime'];
+    	}   
     
-    if ($_SERVER['REMOTE_ADDR'] != $accessIP) {
-        $access = "Bad request at ". date("Y-m-d h:i:s A"). " with ip " . $_SERVER['REMOTE_ADDR'] . "\n";
-        
-        echo($access);
-        
-        $fd = fopen("access.txt", 'a');
-        fwrite($fd, $access);
-        fclose($fd);
-    }else{
-    echo("\nSave started at " . date('i:s'));
+		//save formatted dictionary array to save file
+		save("saves/v2", date('Y-m-d@H'), json_encode($savef), ".json", "w");
     
-    $data = json_decode(file_get_contents('https://ttt-fun.com/staff/?json=1'), true);
-    
-    for ($i = 0; $i < count($data); $i++){
-        $id2time[steamID_steam64($data[$i]['steam'])] = $data[$i]['onlinetime'];
-    }   
-    
-    $fp = fopen("saves/".date('Y-m-d@H'). ".json", 'w');
-    
-    fwrite($fp, json_encode($id2time));
-    fclose($fp);
-    
-    echo("\nSave finished at ". date('i:s') . "\n");
+    	echo("\nSave finished at ". date('i:s') . "\n");
     }
+
+	elseif($_GET["action"] == "log")
+
+	/*
+		IP logging system. Displays IP addresses and counts the times they have connected.
+	*/
+	
+	{
+		//get data from current access file (contains IP address history)
+		$data = file_get_contents("./access.txt");
+		
+		//explode every IP address
+		$data = explode(" ", $data, -1);
+		$finished;
+
+		//Loop through every address and count them
+		for($i = 0; $i < count($data); $i++)
+		{
+			if(isset($finished[$data[$i]]))
+			{
+				$finished[$data[$i]]++;
+			}
+			else
+			{
+				$finished[$data[$i]] = 1;
+			}
+		}
+
+		//echo the counted IPs
+		foreach($finished as $ip => $times)
+		{
+			echo($ip . " : " . $times . "\n");
+		}
+
+}
+	if ($_GET['action'] == "cache")
+		
+		/*
+			Cache Function
+		*/
+		
+	{
+		cache();
+	}
+}
+else{ ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PixelByte | StaffTracker</title>
+    <!-- Written by Pips - pips@pixelbyte.net-->
+    <!-- Animations from animate.css-->
+    <link rel="stylesheet" href="http://pixelbyte.net/public/style_master.css" type="text/css" charset="utf-8">
+    <link rel="stylesheet" href="http://pixelbyte.net/public/animate.css" type="text/css" charset="utf-8">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+	<link rel="stylesheet" href="/resources/demos/style.css">
+	<style>
+table,th,td
+{
+border:3px solid white;
+border-collapse:collapse;
+	text-align: center
+}
+</style>
+	<meta charset="utf-8">
+</head>
+<body>
+    <div id='header'>
+    <?php
+    //header
+    include('./header.php');
+    ?>
+    </div>
+    
+    
+    <div id='page'>
+        <div id='pageTitle'>
+            <h1 class="animated fadeInDown">
+                <center>
+                    StaffTracker
+                </center>
+            </h1>
+			<h3 class="animated fadeIn">
+				<center>
+					Updated display and added multiple user functionality<br><a href="https://github.com/PixelByte/staffTracker">Project's Github</a>
+					
+				</center>
+			</h2>
+        </div>
+        
+        
+        <div id='pageContent'>
+			<?php
+	if ( isset( $_REQUEST['id'] ) ) 
+	{
+		
+		//load name cache
+		$nameCache = json_decode(file_get_contents("./cache.json"), true);
+
+		//expand requested steamID to an array
+		$search = explode(",", $_REQUEST['id']);
+
+		//Get data to compare the first day
+		$data1 = json_decode(file_get_contents("./saves/v2/2014-04-16@16.json"), true);
+		
+		//Loop through the data and create a dictionary
+		for($i = 0; $i < count($data1); $i++)
+		{
+			$day1[$data1[$i]["steamID"]] = $data1[$i]["time"];
+		}
+
+		$data2 = json_decode(file_get_contents("./saves/v2/" . date('Y-m-d@H') . ".json"), true);
+
+	for($i = 0; $i < count($data2); $i++)
+	{
+			$day2[$data2[$i]["steamID"]] = $data2[$i]["time"];
+		}
+
+
+	for($i = 0; $i < count($search); $i++){
+
+	$time[$i] = $day2[$search[$i]] - $day1[$search[$i]];
+
+		echo ("
+			<center>
+			<table style='width:400px'>
+		<h1 class='animated fadeInDown'>". $nameCache[$search[$i]]  ." (".$search[$i].")</h1>
+		</table>
+		
+		<table style='width:600px' class='animated fadeIn'>
+		<tr>
+			<td>time at 2014-04-16@16</td>
+			<td><b>". time_elapsed_A($day1[$search[$i]]) ."</b></td>
+		</tr><tr>
+			<td>time at ".date('Y-m-d@H')."</td>
+			<td><b>". time_elapsed_A($day2[$search[$i]]) ."</b></td>
+		</tr>
+		<tr>
+			<td>difference</td>
+			<td><b>" .time_elapsed_A($time[$i]) . "</b></td>
+		</tr>
+		</table>
+		</table>
+		<br>");
+        }
+// Displays when site is loaded or form isn't filled
+} else {
+	
+		// Display the form
+		echo <<<HTML
+		<form method="post" action="" class="animated fadeInDown">
+		<p><label><center><input autofocus type="text" class="text" name="id" value="" /></center></label></p>
+		</form>	
+HTML;
+
 }
 
-/*
-    Load function. EXAMPLE: http://pb-mc.net/a/?action=load&date=2014-04-16@09
-*/
+			?>
+        </div>
+    </div>
+	
+	<br>
+	
+    <?php
+//footer
+    /*
+                            IMPORTANT
 
-if ($_GET["action"] == "load"){
+    Make sure to fix BOLTH includes before loading the page up.
+    */
+    include('./footer.php'); 
+    ?>
     
-    if (isset($_GET["date"])){
-    echo( file_get_contents("http://pb-mc.net/a/saves/". $_GET["date"] . ".json"));
-    }
-    else{
-        header("Location: http://pb-mc.net/a/saves/");
-    }
-            
-}
-?>
+</body>
+
+</html>	
+
+<?php } ?>
--- methods.php
+++ methods.php
@@ -1,20 +1,108 @@
 <?php
 
 /*
-    Converts $steamID into steam64 and returns it
+    Cacheing function for faster name loading
 */
-function steamID_steam64($steamID){
+
+function cache()
+{
+	
+	//sleep for one minute to eliminate the possibility of un-finished saves.
+	sleep(60);
+
+	//load most current file. Because the cache ans save file are run at the same second, that's why we wait to make sure the file exists before loading it.
+	$data = json_decode(file_get_contents("./saves/v2/" . date("Y-m-d@H") . ".json"), true);
+
+		$steamQuery;
+
+	//define assosiative array (dictonary) and set the values to null to be replaced.
+	for($i = 0; $i < count($data); $i++)
+	{
+		$steamID[$data[$i]["steamID"]] = null;
+	}
+
+	//loop through every assosiative array key and append them to a string.
+	foreach($steamID as $id => $x)
+	{
+		$steamQuery .= $id. ",";
+	}
+
+	//send that string to steam and get profile/playerdata back (method included in this class).
+	$idData = steam64_json($steamQuery);
+
+	//loop through the data that steam returned and append it to an assosiative array in the form of key = 64bitID => value = steam name at time of request.
+	for($i = 0; $i < count($idData); $i++)
+	{
+			   $steamID[(string)$idData[$i]["steamid"]] = $idData[$i]["personaname"];
+	}
+
+	echo("cache started at " . date("H:m") . "<br>\n");
+	
+	//encode and save the data
+	save(".", "cache", json_encode($steamID), ".json", "w");
+	
+	echo("finished cache at ". date("H:m") . "<br>\n cahched " . count($steamID) . " names");
+    }
+
+/*
+	Converts a STEAM_0: ID to a 64bit ID
+*/
+
+function steamID_steam64($steamID)
+{
     
+	//split the data into 3 types, 2 of them we actually need ($type, $id)
      list($e, $type, $id) = split(":", $steamID);
+	
+	//some math to figure out and return the 64 bit ID
     return ($id * 2) + 76561197960265728 + $type;
     
 }
 
 /*
-    Gets player.json from valve with $steam64 then returns it decoded
+	Steam data
 */
-function steam64_json($steam64){
-    return json_decode(file_get_contents('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=CA7B8780E22D27C381BD47BF41565B27&steamids='. strval($steam64)), true);
-    
+
+function steam64_json($steam64)
+{
+	
+	//returns the json data from steam. Can have multiple steam IDs sent if appended in the $steam64 (must be appended with a "," and no breaks)
+	$x = json_decode(file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=CA7B8780E22D27C381BD47BF41565B27&steamids='. strval($steam64)), true);
+    return $x["response"]["players"];
 }
 
+/*
+	Save function. Easier than copying and pasting multiple lines.
+*/
+
+function save($location, $filename, $data, $extention, $action){
+	
+	//example: save("saves", "test", null, ".txt", "w")
+	$fp = fopen( $location . "/" . $filename .  $extention, $action);
+	fwrite($fp, $data);
+    fclose($fp);
+}
+
+/*
+	Takes seconds, converts them into years, weeks, days, hours, minutes
+*/
+
+function time_elapsed_A($secs){
+	
+	if($secs == null){ return false; }
+	
+    $bit = array(
+        'y' => $secs / 31556926 % 12,
+        'w' => $secs / 604800 % 52,
+        'd' => $secs / 86400 % 7,
+        'h' => $secs / 3600 % 24,
+        'm' => $secs / 60 % 60,
+        's' => $secs % 60
+        );
+        
+    foreach($bit as $k => $v)
+        if($v > 0)$ret[] = $v . $k;
+        
+    return join(' ', $ret);
+    }
+    
