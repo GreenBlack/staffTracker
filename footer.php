1e9959e1170e751b236f92b5311906e6409430c8
PixelPips <pips@pixelbyte.net>
2014-05-06 00:40:55 -0600

    Documentation

--- footer.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//animations
-echo"<link rel='stylesheet' type='text/css' href='http://pixelbyte.net/public/animate.css' />";
-
-//style
-echo("<link rel='stylesheet' type='text/css' href='http://pixelbyte.net/public/style_header.css' />");
-
-//footer
-echo("<center><footer class='animated fadeIn' id='footer'>&copy; <a href='http://pixelbyte.net'>Pixelbyte 2014.</a> All rights reserved</footer></center></link></link>"); ?>
